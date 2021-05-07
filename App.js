4932e9ca1daaae18eb0439de3816d1e6719a4a41
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2021-05-07 06:56:20 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {createAppContainer } from 'react-navigation';
-import {createBottomTagNavigator} from 'react-navigation-tabs';
-import {ScanScreen} from './screens/ScanScreen'
+import ScanScreen from './screens/ScanScreen'
 
 export default class App extends React.Component{
   render(){
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <ScanScreen></ScanScreen>
       </View>
     );
   }
 }
 
-const tagNavigator = createBottomTagNavigator({
-  ScanScreen: ScanScreen
-})
 const AppContainer = createAppContainer(tagNavigator)
 const styles = StyleSheet.create({
   container: {
