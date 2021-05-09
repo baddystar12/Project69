dbf9979d3fc55fa680686874cf12e22daeffd0d1
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2021-05-09 15:47:07 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {createAppContainer } from 'react-navigation';
-import ScanScreen from './screens/ScanScreen'
 
-export default class App extends React.Component{
+
+import ScanScreen from './screens/ScanScreen';
+ 
+
+export default class App extends React.Component {
   render(){
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <ScanScreen></ScanScreen>
-      </View>
+        <View style={styles.container}>
+        <ScanScreen />
+        </View>
+        
     );
   }
 }
 
-const AppContainer = createAppContainer(tagNavigator)
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
